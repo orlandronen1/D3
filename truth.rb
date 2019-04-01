3ef1f73726bbfd126e0fc839020fb330378c7e2f
Ronen Orland <roo18@pitt.edu>
2019-04-01 11:19:53 -0400

    Erbs
    Made erbs/stubs, filled out index.erb. Should have the initial form as stated by the reqs.

--- truth.rb
+++ truth.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+@truth_symbol = 'T'
+@false_symbol = 'F'
+@size = 3
+
 # Do logical AND
 def and(t, f)
 
@@ -28,3 +32,16 @@ end
 # => what it does
 # => erb :erbname
 # end
+
+# What to do if route cannot be found
+not_found do
+  status 404
+  # erb :error
+end
+
+# Main page
+get '/' do
+  erb :index
+end
+
+# Post requests
