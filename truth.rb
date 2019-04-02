50caeb0d8004632d4db9e79831dce24a168f006e
Ronen Orland <roo18@pitt.edu>
2019-04-01 22:47:23 -0400

    Cleaned up truth.rb
    Removed unused methods and some comments.

--- truth.rb
+++ truth.rb
@@ -5,41 +5,6 @@ require 'sinatra/reloader'
 @false_symbol = 'F'
 @size = 3
 
-# Do logical AND
-def log_and(t, f, num_false)
-  if num_false > 0
-    f
-  else
-    t
-  end
-end
-
-# Do logical OR
-def log_or(t, f, num_true)
-  if num_true > 0
-    t
-  else
-    f
-  end
-end
-
-# Do logical NAND
-def nand(t, f, num_false)
-  not log_and(t, f)
-end
-
-# Do logical NOR
-def nor(t, f, num_true)
-  not log_or(t, f)
-end
-
-
-# Template
-# get '/path' do
-# => what it does
-# => erb :erbname
-# end
-
 # What to do if route cannot be found
 not_found do
   status 404
@@ -57,10 +22,12 @@ end
 post '/' do
   puts "Truth: #{params['t']}, False: #{params['f']}, Size: #{params["size"]}"
   
+  # Update vars to either input values, if provided, or defaults if not
   params['t'].nil? or params['t'].empty? ? @truth_symbol = 'T' : @truth_symbol = params['t']
   params['f'].nil? or params['f'].empty?  ? @false_symbol = 'F' : @false_symbol = params['f']
   params["size"].nil? or params["size"].empty? ? @size = 3 : @size = params["size"].to_i
 
+  # Check for invalid inputs
   if @truth_symbol == @false_symbol or
         @truth_symbol.length != 1 or 
         @false_symbol.length != 1 or
